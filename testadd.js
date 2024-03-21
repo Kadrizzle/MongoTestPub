3453b766aa45df68ca284d9861ada6c804d6cff2
Kadrizzle <lanekade8@gmail.com>
2024-03-21 00:06:46 -0500

    correct connection string

--- testadd.js
+++ testadd.js
@@ -2,7 +2,7 @@ const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
 const uri =
-  "mongodb+srv://Kadazzle:kadizzleinthehizzle@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://Kadazzle:kadizzleinthehizzle@cluster0.xgaaecl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require("express");
